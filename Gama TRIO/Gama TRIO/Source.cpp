bdfc35fca23e6d8f36b8d0155cdc1576a18c644a
pfefferberh <pfefferberhorion@gmail.com>
2020-01-17 00:55:56 +0200

    test war fix bags

--- Gama TRIO/Gama TRIO/Source.cpp
+++ Gama TRIO/Gama TRIO/Source.cpp
@@ -14,7 +14,6 @@
 //	Не забудьте здійснити приведення типів там, де це необхідно.
 //
 //
-
 #include <iostream>
 #include <string>
 #include <ctime>
@@ -28,7 +27,10 @@ public:
 	int health;
 	int damage;
 	int chans;
-	virtual void Fill() =0;
+	
+	static void show(unit team_one, unit team_two);
+	void Set_health(int health) { this->health = health; };
+	int Get_health() { return this->health; };
 	unit() {};
 	~unit() {};
 };
@@ -36,105 +38,111 @@ public:
 class Swords :public unit
 {
 public:
-	Swords() { Fill(); };
-	~Swords() {};
-	void Fill ()override {
+	Swords() {
 		this->name = "Swords";
 		this->damage = 5;
 		this->health = 15;
 		this->chans = 60;
-	}
+	};
+	~Swords() {};
 };
 class Archer :public unit
 {
 public:
-	Archer() { Fill(); };
-	~Archer() {};
-	void Fill()override {
+	Archer() {
 		this->name = "Archer";
 		this->damage = 10;
 		this->health = 8;
 		this->chans = 30;
 	};
+	~Archer() {};
 };
 class Withard :public unit
 {
 public:
-	Withard() { Fill(); };
-	~Withard() {};
-	void Fill()override {
+	Withard() {
 		this->name = "Withard";
 		this->damage = 4;
 		this->health = 12;
 		this->chans = 40;
 	};
+	~Withard() {};
 };
 
-class Player {
-	string name;
-	int health;
-public:
-	void Fill(unit* vehicles) {
-		vehicles->Fill();
-		this->name=vehicles->name;
-		this->health =vehicles->health;
-	}
-	string Get_name() { return this->name; };
-	int Get_health() { return this->health; };
-};
-void war(unit* team_one, unit* team_two)
+void unit::show(unit team_one, unit team_two)
 {
-	//show(team_one,team_two);
-	if (rand() % 100 > team_one->chans) {
-		team_one->health = team_one->health - team_two->damage;
-		cout << team_two->name << " attacked " << team_one->name << endl;
-		cout << "Health " << team_one->name << ": " << team_one->health << endl;
-	}
-
-	if (rand() % 100 > team_two->chans) {
-		team_two->health = team_two->health - team_one->damage;
-		cout << team_one->name << " attacked " << team_two->name << endl;
-		cout << "Health " << team_two->name << ": " << team_two->health << endl;
-	}
-
+	cout << team_two.name << " Health: " << team_two.health << endl;
+	cout << team_one.name << " Health: " << team_one.health << endl;
 }
-void show(Player team_one, Player team_two)
+void war(unit &team_one, unit &team_two);
+void war(unit& team_one, unit  &team_two)
 {
-	cout << team_two.Get_name() << " Health: " << team_two.Get_health() << endl;
-	cout << team_one.Get_name() << " Health: " << team_one.Get_health() << endl;
+	//show(team_one, team_two);
+	int chans_random = rand() % 100;
+	cout << "-------------->>------------" << endl;
+	if (chans_random > team_one.chans) {
+		cout << team_two.name << " attacked " << team_one.name << endl;
+		cout << "Health " << team_one.name <<": "<< team_one.Get_health()<<" - "<< team_two.name<<" damage: "<<team_two.damage<<endl;
+		team_one.Set_health(team_one.Get_health() - team_two.damage);
+		cout << "Health " << team_one.name << ": " << team_one.Get_health() << endl;
+		if (team_one.Get_health()<=0) {
+			cout << " Die " << endl;
+		}
+	}
+	else {
+		cout << " Blocking fate " << endl;
+	}
+	cout << "--------------<<------------" << endl;
+	/*if (rand() % 100 > team_two.chans) {
+		team_two.health = team_two.health - team_one.damage;
+		cout << team_two.health << " - " << team_one.damage << endl;
+		cout << team_one.name << " attacked " << team_two.name << endl;
+		cout << "Health " << team_two.name << ": " << team_two.health << endl;
+	}*/
 }
 
-void Fill_army(Player* army, int size) {
-	Swords swords;
-	Archer archer;
-	Withard withard;
+void Fill_army(unit* army, int size) {
 	int chans;
 	for (int i = 0; i < size; i++) {
 		chans = rand() % 3;
 		if (chans == 1) {
-			army[i].Fill(&swords);
+			army[i] = Swords();
 		}
 		else if (chans == 2) {
-			army[i].Fill(&archer);
+			army[i] = Archer();
 		}
 		else {
-			army[i].Fill(&withard);
+			army[i] = Withard();
 		}
 	}
 	for (int i = 0; i < size; i++) {
-		cout << i + 1 << army[i].Get_name() << endl;
+		cout << i + 1 << army[i].name << endl;
 	}
 }
 int main() {
 	srand(unsigned(time(NULL)));
 	const int size = 3;
-	Player orc[size];
-	Player man[size];
-	Fill_army(orc,size);
-	Fill_army(man,size);
-	for (int i = 0;i<3;i++) {
-		show(orc[i],man[i]);
+	int choise_aliance;
+	int choise_enemy;
+	unit* orc = new unit[size];
+	unit* man = new unit[size];
+	cout << "`````````````````````````Team 1``````````````````" << endl;
+	Fill_army(orc, size);
+	cout << "`````````````````````````Team 2``````````````````" << endl;
+	Fill_army(man, size);
+	cout << "`````````````````````````war``````````````````" << endl;
+	for (int i = 0;i<10;i++) {
+		cout << "Choise your unit: ";
+		cin >> choise_aliance;
+		cout << "Choise enemy unit: ";
+		cin >> choise_enemy;
+		if (man[choise_aliance-1].health > 0 ) {
+			war(man[choise_aliance-1], orc[choise_enemy-1]);
+		}
 	}
+	
+	delete orc;
+	delete man;
 	system("pause");
 	return 0;
 }
