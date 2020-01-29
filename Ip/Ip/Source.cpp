4db0a3f61a34f5b309c4af9a818b3d5921834fef
pfefferberh <pfefferberhorion@gmail.com>
2020-01-29 16:45:38 +0200

    help me

---
+++ Ip/Ip/Source.cpp
@@ -0,0 +1,58 @@
+//Дано рядок(С або С++ на вибір).Перевірити чи він містить вірно записану IP - адресу.
+//Використати рядкові потоки.Наприклад, 255.255.255.0 - вірна, 255.255.0.256, 192.0.157 –
+//невірні.
+#include<iostream>
+#include<string>
+using namespace std;
+
+void CheckToken(string token) {
+	int number = stoi(token);
+	for (int i = 0; i < token.length(); i++) {
+		if (int(token[i]) <= 45 && int(token[i]) >= 58)
+		{
+			throw exception("Ip not correct");
+		}
+
+	}
+	if (number == 0 && token.length() > 1) { throw exception("Ip not correct"); }
+	if (number < 0 || number > 255 || token.length() > 3) { throw exception("Ip not correct"); }
+}
+
+int main() {
+	string ip;
+	string token;
+
+	int prevIndex = 0;
+	int tokenCounter = 0;
+
+	cout << "Enter Ip" << endl;
+	cin >> ip;
+
+	for (int i = 0; i < ip.length(); i++) {
+		if (ip[i] == '.') {
+			token = ip.substr(prevIndex, i - prevIndex);
+			tokenCounter++;
+			prevIndex = i + 1;
+
+			if (tokenCounter > 3) {
+				cout << "Error( invalid IP )" << endl;
+				break;
+			}
+
+			try {
+				CheckToken(token);
+			}
+			catch (...) {
+				cout << "Error( invalid IP )" << endl;
+				break;
+			}
+		}
+		if (i == ip.length() - 1) {
+			token = ip.substr(prevIndex, i - prevIndex + 1);
+			CheckToken(token);
+			cout << "Ip correct" << endl;
+		}
+	}
+	system("pause");
+	return 0;
+}
